0db531c327d707393c18b9eef76a2851030504d7
Kennedy Silveira <kennedysilveira.ks@gmail.com>
2021-11-17 22:25:15 -0300

    primeiro commit

---
+++ CODAI/js/index.js
@@ -0,0 +1,30 @@
+var nome = "Kennedy Silveira";
+var cargo = "CTO-GrowDev";
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+
+function colocarNomeNoHtml(nome){
+    nomeHtml.innerHTML = nome;
+}
+function colocarCargoNoHtml(cargo){
+    cargoHtml.innerHTML = cargo;
+
+}
+function logarNome(){
+    console.log(nome);
+}
+function clickNoBotaoProjetos(){
+    console.log("clicou no botao projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+function clickNoSobre(){
+   console.log("Clicou no botao sobre");
+   texto1.style.display = "block";
+   texto2.style.display = "none";
+}
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
